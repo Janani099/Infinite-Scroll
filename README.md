20b16d1d40bfe6e12ba92c4723d336751ce98c5c
janani099 <jananicse.senthil@gmail.com>
2022-05-12 00:43:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Infinite Scroll React Example Using React-Infinite-Scroll-Component
---
+++ src/components/Comment.js
@@ -0,0 +1,14 @@
+
+
+function Comment({item: { id }}) {
+    return (
+        
+        <div className="text-body">
+          <h5 className="title h2">Id : {id}</h5>
+         <p>This is inifinte scroll</p>
+        </div>
+    
+    )
+}
+
+export default Comment
